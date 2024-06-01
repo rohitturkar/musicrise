02bd063121689ef43cd32c624d2f24245ee2332a
rohitturkar <rohittur2002@gmail.com>
2024-06-01 14:48:59 +0530

    solve error comes during build

--- src/Store/wishlistSlice.ts
+++ src/Store/wishlistSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import toast from "react-hot-toast";
 
 // Define the type for a wishlist item
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
